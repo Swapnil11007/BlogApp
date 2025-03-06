bf64f14c9efbb3558c3c02a90be21d00c1cbd7b9
Swapnil11007 <shindes12121@gmail.com>
2025-03-06 19:01:14 +0400

    added filter and bell btn

--- app/src/components/SearchBar.jsx
+++ app/src/components/SearchBar.jsx
@@ -1,32 +1,71 @@
-import { View, Text, TextInput, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 
 const SearchBar = ({ blogData, setFilteredData }) => {
   return (
-    // <View>
-    <TextInput
-      placeholder="Search"
-      style={styles.searchBarStyle}
-      onChangeText={(searchText) => {
-        // console.log('searchText ---',blogData)
-        const filteredData = blogData?.filter(
-          (blog) =>
-            blog?.title.includes(searchText) || blog.body.includes(searchText)
-        );
-        setFilteredData(filteredData);
+    <View
+      style={{
+        flexDirection: "row",
+        gap: 10,
+        alignItems: "center",
+        justifyContent: "space-between",
       }}
-      
-    />
-    // </View>
+    >
+      <View style={styles.searchBarStyle}>
+        <TextInput
+          placeholder="Search..."
+          style={styles.inputContainer}
+          onChangeText={(searchText) => {
+            // console.log('searchText ---',blogData)
+            const filteredData = blogData?.filter(
+              (blog) =>
+                blog?.title
+                  .toLowerCase()
+                  .includes(searchText.toLocaleLowerCase()) ||
+                blog.body
+                  .toLocaleLowerCase()
+                  .includes(searchText.toLocaleLowerCase())
+            );
+            setFilteredData(filteredData);
+          }}
+        />
+        <TouchableOpacity style={styles.filterContainer}>
+          <Text>Filter</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity style={styles.bellContainer}>
+        <Text>Bell</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   searchBarStyle: {
-    width: "80%",
+    flex: 1,
+    flexDirection: "row",
     backgroundColor: "#rgb(214, 206, 206)",
     borderRadius: 20,
+    overflow: "hidden",
+    gap: 20,
+    // paddingHorizontal: 20,
+  },
+  inputContainer: { flex: 1, paddingStart: 20 },
+  filterContainer: {
     padding: 10,
+    backgroundColor: "pink",
+  },
+  bellContainer: {
+    backgroundColor: "pink",
+    alignSelf: "stretch",
+    padding: 10,
+    borderRadius: 100,
   },
 });
 
